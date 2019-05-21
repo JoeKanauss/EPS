80619d3945d151139f6fe0d675ef203d0756065d
Joseph Kanauss <joekanauss@gmail.com>
2019-05-21 15:16:39 -0500

    initial commit

---
+++ js/EPS-js.js
@@ -0,0 +1,9 @@
+
+
+//onclick of hamburger menu, menu comes in from the sidebar
+function showMenu(){
+	$("#sidebar").toggle("fold", 500);
+}
+function hideMenu(){
+	$("#sidebar").toggle("drop");
+}
